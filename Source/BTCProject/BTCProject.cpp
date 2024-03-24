0da3eecfa1f0be42a12bb283b5909bf58140bbfc
pobertt <79865756+pobertt@users.noreply.github.com>
2024-03-24 12:27:42 +0000

    creating project

---
+++ Source/BTCProject/BTCProject.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "BTCProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BTCProject, "BTCProject" );
+ 
---
+++ Source/BTCProject/BTCProjectGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "BTCProjectGameMode.h"
+#include "BTCProjectCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+ABTCProjectGameMode::ABTCProjectGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
