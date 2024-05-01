0612c2f8f7af2a1405cd5649d8c844315d9998d2
pobertt <79865756+pobertt@users.noreply.github.com>
2024-05-01 18:05:22 +0100

    changed default pawn class

--- Source/BTCProject/BTCProjectGameMode.cpp
+++ Source/BTCProject/BTCProjectGameMode.cpp
@@ -7,7 +7,7 @@
 ABTCProjectGameMode::ABTCProjectGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Player/BP_PlayerCharacter"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
